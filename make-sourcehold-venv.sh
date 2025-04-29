a6b9a94782c4370dec830e72288a1c898c167f15
kolunmi <kolunmi@posteo.net>
2025-04-29 09:18:03 -0700

    all the zlibs

--- make-sourcehold-venv.sh
+++ make-sourcehold-venv.sh
@@ -12,9 +12,10 @@ echo '-------- START BUILDING SOURCEHOLD --------'
 
 # SOURCEHOLD_VERSION="$("$PYTHON" "$MESON_SOURCE_ROOT"/sourcehold-maps/setup.py -V 2>/dev/null)"
 "$PYTHON_EXE" -m venv "$OUTPUT"
-"$OUTPUT"/bin/python -m pip -v install --upgrade pip
-"$OUTPUT"/bin/python -m pip -v install build
-"$OUTPUT"/bin/python -m build "$SOURCE_ROOT"/sourcehold-maps -v -w -o "$PRIVATE_DIR"
-"$OUTPUT"/bin/python -m pip -v install --prefer-binary "$PRIVATE_DIR"/*.whl
+"$OUTPUT"/bin/python3 -m pip -v install --upgrade pip
+CFLAGS="-I/mingw64/include" "$OUTPUT"/bin/python3 -m pip install --upgrade Pillow --no-binary :all: -C parallel="$(nproc)" -C platform-guessing=disable
+"$OUTPUT"/bin/python3 -m pip -v install build
+"$OUTPUT"/bin/python3 -m build "$SOURCE_ROOT"/sourcehold-maps -v -w -o "$PRIVATE_DIR"
+"$OUTPUT"/bin/python3 -m pip -v install "$PRIVATE_DIR"/*.whl
 
 echo '-------- END BUILDING SOURCEHOLD --------'
