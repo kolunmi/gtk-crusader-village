7948c8fcb30f5ac4fdbb559d9dc2aa930620b559
kolunmi <kolunmi@posteo.net>
2025-04-30 14:40:58 -0700

    fixing ci again

--- make-sourcehold-install.sh
+++ make-sourcehold-install.sh
@@ -16,21 +16,31 @@ rm -rf "$OUTPUT"
 
 if [ "$HOST_SYSTEM" = 'windows' ]; then
 
-    EMBEDDED_PYTHON_VER='3.13.3'
-    EMBEDDED_PYTHON_ZIP='python-'"$EMBEDDED_PYTHON_VER"'-embed-amd64.zip'
-    EMBEDDED_PYTHON_URL='https://www.python.org/ftp/python/'"$EMBEDDED_PYTHON_VER"'/'"$EMBEDDED_PYTHON_ZIP"
-    wget -O "$PRIVATE_DIR"/"$EMBEDDED_PYTHON_ZIP" "$EMBEDDED_PYTHON_URL"
-    unzip "$PRIVATE_DIR"/"$EMBEDDED_PYTHON_ZIP" -d "$OUTPUT"
+    PYTHON_INSTALL_VERSION='3.13.3'
+    PYTHON_INSTALL_BUILD_DATE='20250409'
+    PYTHON_INSTALL_TAR='cpython-'"$PYTHON_INSTALL_VERSION"'+'"$PYTHON_INSTALL_BUILD_DATE"'-x86_64-pc-windows-msvc-install_only.tar.gz'
+    PYTHON_INSTALL_URL='https://github.com/astral-sh/python-build-standalone/releases/download/'"$PYTHON_INSTALL_BUILD_DATE"'/'"$PYTHON_INSTALL_TAR"
+    wget -O "$PRIVATE_DIR"/"$PYTHON_INSTALL_TAR" "$PYTHON_INSTALL_URL"
+    tar -C "$PRIVATE_DIR" -xzvf "$PRIVATE_DIR"/"$PYTHON_INSTALL_TAR"
+    mv "$PRIVATE_DIR"/python "$OUTPUT"
 
-    mv "$OUTPUT"/python313._pth "$OUTPUT"/python313.pth
-    mkdir -p "$OUTPUT"/DLLS
+    # PYTHON_INSTALL_SRC='Python-'"$PYTHON_INSTALL_VERSION"
+    # PYTHON_INSTALL_SRC_TAR="$PYTHON_INSTALL_SRC"'.tar.xz'
+    # PYTHON_INSTALL_SRC_URL='https://www.python.org/ftp/python/'"$PYTHON_INSTALL_VERSION"'/'"$PYTHON_INSTALL_SRC_TAR"
+    # wget -O "$PRIVATE_DIR"/"$PYTHON_INSTALL_SRC_TAR" "$PYTHON_INSTALL_SRC_URL"
+    # tar -C "$PRIVATE_DIR" -xJf "$PRIVATE_DIR"/"$PYTHON_INSTALL_SRC_TAR"
+    # cp -r "$PRIVATE_DIR"/"$PYTHON_INSTALL_SRC"/Include "$OUTPUT"/Include
+    # cp "$PRIVATE_DIR"/"$PYTHON_INSTALL_SRC"/PC/pyconfig.h.in "$OUTPUT"/Include/pyconfig.h
 
-    GETPIP_URL='https://bootstrap.pypa.io/get-pip.py'
-    wget -O "$PRIVATE_DIR"/get-pip.py "$GETPIP_URL"
-    "$OUTPUT"/python.exe "$PRIVATE_DIR"/get-pip.py
+    # mv "$OUTPUT"/python313._pth "$OUTPUT"/python313.pth
+    # mkdir -p "$OUTPUT"/DLLS
+
+    # GETPIP_URL='https://bootstrap.pypa.io/get-pip.py'
+    # wget -O "$PRIVATE_DIR"/get-pip.py "$GETPIP_URL"
+    # "$OUTPUT"/python.exe "$PRIVATE_DIR"/get-pip.py
     
     "$OUTPUT"/python.exe -m pip install --upgrade pip
-    "$OUTPUT"/python.exe -m pip install build distlib setuptools pybind11
+    "$OUTPUT"/python.exe -m pip install build distlib setuptools
     "$OUTPUT"/python.exe -m build "$SOURCE_ROOT"/sourcehold-maps -w -o "$PRIVATE_DIR"
     "$OUTPUT"/python.exe -m pip install --upgrade "$PRIVATE_DIR"/*.whl
     
